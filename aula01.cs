b80d1fa16617d74b60943b6e26c60cf8abd5d59a
Gabriel9rocha <106763087+Gabriel9rocha@users.noreply.github.com>
2023-02-17 18:02:53 -0300

    Add files via upload
    recebendo dados e imprimindo eles na tela

---
+++ aula01.cs
@@ -0,0 +1,15 @@
+using System;
+class principal{
+
+    static void Main()
+    {
+        
+        Console.WriteLine("digite ola mundo em ingles:    ");
+        Console.ReadLine( );
+       
+       
+        Console.Write("hello world ");
+       
+        //print-f em c#
+    }
+}
